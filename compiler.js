4e2d82daae9c35699b6ff1ee6fb57077c63c534b
Mathias Menzel-Nielsen <mathias.menzel-nielsen@mondiamedia.com>
2015-01-20 11:26:05 +0100

    geilo

--- compiler.js
+++ compiler.js
@@ -6,19 +6,34 @@ var clone = function(obj){
     return JSON.parse(JSON.stringify(obj));
 }
 
-var opcodes = {
-    $copy: function(options, root){
-        root[options.to] = _.clone(root[options.from]);
-    },
-    $delete: function(options, root){
-        delete root[options];
-    }
-};
-
 var runCommand = function(code, root){
     var command  = _.first(_.keys(code));
     opcodes[command](code[_.keys(code)[0]], root);
 }
+
+var opcodes = {
+    $copy: function(options, root){
+        if(options.value){
+            root[options.to] = options.value;
+        }else{
+            root[options.to] = _.clone(root[options.from]);
+        }
+    },
+    $delete: function(options, root){
+        delete root[options];
+    },
+    $map: function(options, root) {
+        var doables = options["do"];
+        var result = _.map(root[options.from], function(element){
+            _.each(doables, function(doable){
+                runCommand(doable, element);
+            });
+            return element;
+        });
+        root[options.to] = result;
+    }
+};
+
 
 var compile = function(code){
     return function(root){
@@ -39,7 +54,8 @@ var testcode = require('./test/sample.json');
 
 var compiled = compile(testcode);
 
-console.log(compiled({source: "hallo"}));
-
+console.log(compiled({source: {wert: "hallo"},
+                      mappable: [{von: "hier"},
+                                 {von: "hier auch"}]}));
 
 

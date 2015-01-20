0884acffbb4c8279c55452bfefe485ffdb322c15
Oliver Voeltz <oliver.voeltz@mondiamedia.com>
2015-01-20 14:58:20 +0100

    Dot pathes. New opcodes copyOne and comment

--- compiler.js
+++ compiler.js
@@ -1,4 +1,6 @@
 var _= require('underscore');
+// https://www.npmjs.com/package/mpath
+var mpath = require('mpath');
 
 
 var clone = function(obj){
@@ -11,13 +13,21 @@ var runCommand = function(code, root){
 }
 
 var opcodes = {
+    /*
+     "$copy": {"from": "source", "to": "target"}
+     "$copy": {"value": "Mondia Media", "to": "target.vendor"}
+     */
     $copy: function(options, root){
         if(options.value){
-            root[options.to] = options.value;
+            mpath.set(options.to, options.value, root);
         }else{
-            root[options.to] = _.clone(root[options.from]);
+            mpath.set(options.to, _.clone(mpath.get(options.from, root)), root);
         }
     },
+    /*
+     "$delete": "dimension"
+     "$delete": "dimension.width"
+     */
     $delete: function(options, root){
         delete root[options];
     },
@@ -25,15 +35,39 @@ var opcodes = {
         this["$copy"](options, root);
         this["$delete"](options.from, root);
     },
+    /*
+     "$map": {
+         "from": "sourceArray", 
+         "to": "targetArray", 
+         "do": [
+             {"$move": {"from": "FROM", "to": "TO"}},
+             {"$delete": "FIELD"}
+         ]}}
+     */
     $map: function(options, root) {
         var doables = options["do"];
-        var result = _.map(root[options.from], function(element){
+        var result = _.map(mpath.get(options.from, root), function(element){
             _.each(doables, function(doable){
                 runCommand(doable, element);
             });
             return element;
         });
         root[options.to] = result;
+    },
+    /*
+      "$copyOne": { "from": "colorCodes", "to": "colors.colorRed", "filter": { "name": "RED" }}
+     */
+    $copyOne: function(options, root) {
+        var element = _.findWhere(mpath.get(options.from, root), options.filter);
+        if (element != undefined) {
+            this["$copy"]({value: element, to: options.to}, root);
+        }
+    },
+    /*
+     "$comment": "My comment"
+     */
+    $comment: function(options, root) {
+        // Do nothing
     }
 };
 
@@ -47,6 +81,7 @@ var compile = function(code){
         }else{
             runCommand(code, root);
         }
+        //console.log(root);
         return root;
     }
 };
--- test/test.js
+++ test/test.js
@@ -9,14 +9,37 @@ var compiled = compile(testcode);
 
 var result = compiled({source: {wert: "hallo"},
                        mappable: [{von: "hier"},
-                                  {von: "hier auch"}]});
+                                  {von: "hier auch"}],
+
+                       colorCodes: [
+                           {
+                               name: "RED",
+                               rgb: "0xff0000"
+                           },
+                           {
+                               name: "BLUE",
+                               rgb: "0x0000ff"
+                           }
+                       ]
+                      });
 describe('basic tests', function(){
     it('should run all expects', function(done){
         expect(result).toExist();
         
         expect(result.source).toNotExist();
         
-        expect(result.target).toExist().toEqual({wert: "hallo"});
+        expect(result.target).toExist().toEqual({
+            wert: "hallo",
+            vendor: "DPA",
+            wertclone: { wert: "hallo" }
+        });
+
+        expect(result.colors).toExist().toEqual({
+            colorRed: {name: "RED", rgb: "0xff0000"},
+            colorBlue: {name: "BLUE", rgb: "0x0000ff"}
+        });
+
+        
         done();
     });
 });
